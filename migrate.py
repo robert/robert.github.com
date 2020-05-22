aff24d754040d952b6c5715db7c110c178a3c8ec
Robert Heaton <bobjheaton@gmail.com>
2020-05-22 10:17:00 +0100

    w

--- migrate.py
+++ migrate.py
@@ -4,6 +4,8 @@ posts_dir = "./_posts"
 
 for fname in os.listdir(posts_dir):
     path = os.path.join(posts_dir, fname)
+    if path.endswith(".swp"):
+        continue
 
     bits = fname.split("-", 3)
     url = f"/{bits[0]}/{bits[1]}/{bits[2]}/{bits[3].rstrip('.md')}"
